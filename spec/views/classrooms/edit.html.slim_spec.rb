c0ab034828277848f2d8f79531ee1f321fafc85b
Serena Fernandes <serenafribeiro@gmail.com>
2016-05-01 02:59:53 -0300

    Adiciona combo ao classroom

--- spec/views/classrooms/edit.html.slim_spec.rb
+++ spec/views/classrooms/edit.html.slim_spec.rb
@@ -2,10 +2,13 @@ require 'rails_helper'
 
 RSpec.describe "classrooms/edit", type: :view do
   before(:each) do
-    @classroom = assign(:classroom, Classroom.create!(
-      :student => nil,
-      :course => nil
-    ))
+    create(:classroom)
+    create(:course)
+    create(:course)
+    create(:course)
+    create(:student)
+    create(:student)
+    create(:student)
   end
 
   it "renders the edit classroom form" do
@@ -13,9 +16,10 @@ RSpec.describe "classrooms/edit", type: :view do
 
     assert_select "form[action=?][method=?]", classroom_path(@classroom), "post" do
 
-      assert_select "input#classroom_student[name=?]", "classroom[student]"
-
-      assert_select "input#classroom_course[name=?]", "classroom[course]"
+      assert_select "select#classroom_student[name=?]", "classroom[student]"
+      assert_select "select#classroom_course[name=?]", "classroom[course]"
+      assert_select "select#classroom_course>option", :count => 3
+      assert_select "select#classroom_student>option", :count => 3
     end
   end
 end
--- spec/views/classrooms/new.html.slim_spec.rb
+++ spec/views/classrooms/new.html.slim_spec.rb
@@ -2,10 +2,13 @@ require 'rails_helper'
 
 RSpec.describe "classrooms/new", type: :view do
   before(:each) do
-    assign(:classroom, Classroom.new(
-      :student => nil,
-      :course => nil
-    ))
+    create(:classroom)
+    create(:course)
+    create(:course)
+    create(:course)
+    create(:student)
+    create(:student)
+    create(:student)
   end
 
   it "renders new classroom form" do
@@ -13,9 +16,10 @@ RSpec.describe "classrooms/new", type: :view do
 
     assert_select "form[action=?][method=?]", classrooms_path, "post" do
 
-    assert_select "input#classroom_student[name=?]", "classroom[student]"
-
-    assert_select "input#classroom_course[name=?]", "classroom[course]"
+      assert_select "select#classroom_student[name=?]", "classroom[student]"
+      assert_select "select#classroom_course[name=?]", "classroom[course]"
+      assert_select "select#classroom_course>option", :count => 3
+      assert_select "select#classroom_student>option", :count => 3
     end
   end
 end
